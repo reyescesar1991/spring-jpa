9c4c556c0a804727f537d26351bf362056763976
reyescesar0711@gmail.com <reyescesar0711@gmail.com>
2026-01-21 14:10:09 -0400

    FirstCommit

---
+++ src/main/java/com/example/databaseJPA/DatabaseJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.databaseJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/databaseJPA/models/Author.java
@@ -0,0 +1,61 @@
+package com.example.databaseJPA.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @Getter
+// @Setter 
+@NoArgsConstructor
+// @AllArgsConstructor
+@Data //Genera getters y setters automaticamente, equals, hashcode, toString y un constructor
+@Entity
+public class Author {
+
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer age;
+
+
+    // Getters and setters
+
+    // public Integer getId() {
+    //     return id;
+    // }   
+
+    // public String getFirstName() {
+    //     return firstName;
+    // }
+
+    // public void setFirstName(String firstName) {
+    //     this.firstName = firstName;
+    // }
+
+    // public String getLastName() {
+    //     return lastName;
+    // }
+
+    // public void setLastName(String lastName) {
+    //     this.lastName = lastName;
+    // }
+
+    // public Integer getAge() {
+    //     return age;
+    // }
+
+    // public void setAge(Integer age) {
+    //     this.age = age;
+    // }
+
+    // public String getEmail() {
+    //     return email;
+    // }
+
+    // public void setEmail(String email) {
+    //     this.email = email;
+    // }
+}
